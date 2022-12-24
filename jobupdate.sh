7978da01940035714a27057ea2e0467e7a7cd97d
divakar <dvkr22@gmail.com>
2022-12-24 12:13:57 +0530

    Updated

--- jobupdate.sh
+++ jobupdate.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-jenkins-jobs --conf /root/jenkins_jobs.ini update /root/git/jenkins-job-builder/
+jjb="/root/.local/bin/jenkins-jobs --conf /root/jenkins_jobs.ini update /root/git/jenkins-job-builder/jobs/jobs.yaml"
+
+cd /root/git/jenkins-job-builder/jobs/
+
+$jjb
+
+echo $jjb
+
+exit
