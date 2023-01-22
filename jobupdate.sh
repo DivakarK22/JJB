7ac3d4935d630474e8e9aafc48e6e4f1bd5be405
divakar <dvkr22@gmail.com>
2023-01-22 13:02:45 +0530

    Updated

--- jobupdate.sh
+++ jobupdate.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
+delete="/root/.local/bin/jenkins-jobs --conf /root/jenkins_jobs.ini delete /root/git/jenkins-job-builder/jobs/jobs.yaml"
 jjb="/root/.local/bin/jenkins-jobs --conf /root/jenkins_jobs.ini update /root/git/jenkins-job-builder/jobs/jobs.yaml"
-jjbsensu="/root/.local/bin/jenkins-jobs --conf /root/jenkins_jobs.ini update /root/git/jenkins-job-builder/jobs/sensu.yaml"
 
 cd /root/git/jenkins-job-builder/jobs/
 
+$delete
 $jjb
-$jjbsensu
 
 echo $jjb
 
