65cb62f8deef7e9845a0d98d911faed49f2164aa
divakar <dvkr22@gmail.com>
2023-01-22 12:49:16 +0530

    added

--- jobupdate.sh
+++ jobupdate.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 jjb="/root/.local/bin/jenkins-jobs --conf /root/jenkins_jobs.ini update /root/git/jenkins-job-builder/jobs/jobs.yaml"
-jjb-sensu="/root/.local/bin/jenkins-jobs --conf /root/jenkins_jobs.ini update /root/git/jenkins-job-builder/jobs/sensu.yaml"
+jjbsensu="/root/.local/bin/jenkins-jobs --conf /root/jenkins_jobs.ini update /root/git/jenkins-job-builder/jobs/sensu.yaml"
 
 cd /root/git/jenkins-job-builder/jobs/
 
 $jjb
-$jjb-sensu
+$jjbsensu
 
 echo $jjb
 
