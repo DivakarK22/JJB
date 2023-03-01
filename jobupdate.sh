2eed00164635e6059eb3fd9f60133b9b91aa263c
Divakar <dvkr22@gmail.com>
2023-03-01 22:06:10 +0530

    updated

--- jobupdate.sh
+++ jobupdate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-delete="/root/.local/bin/jenkins-jobs --conf /root/jenkins_jobs.ini delete /root/git/jenkins-job-builder/jobs/jobs.yaml"
-jjb="/root/.local/bin/jenkins-jobs --conf /root/jenkins_jobs.ini update /root/git/jenkins-job-builder/jobs/jobs.yaml"
+delete="/root/local/bin/jenkins-jobs --conf /root/git/jenkins-job-builder/jenkins_jobs.ini delete /root/git/jenkins-job-builder/jenkins-job-builder/jobs/jobs.yaml"
+jjb="/root/local/bin/jenkins-jobs --conf /root/git/jenkins-job-builder/jenkins_jobs.ini update /root/git/jenkins-job-builder/jenkins-job-builder/jobs/jobs.yaml"
 
 cd /root/git/jenkins-job-builder/jobs/
 
