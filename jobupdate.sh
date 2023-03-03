d794ddfee9d3d6719eddc2e36e731c7b7259c10e
Divakar <dvkr22@gmail.com>
2023-03-04 00:34:47 +0530

    SYS-8

--- jobupdate.sh
+++ jobupdate.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-delete="/root/local/bin/jenkins-jobs --conf /root/git/jenkins-job-builder/jenkins_jobs.ini delete /root/git/jenkins-job-builder/jenkins-job-builder/jobs/jobs.yaml"
-jjb="/root/local/bin/jenkins-jobs --conf /root/git/jenkins-job-builder/jenkins_jobs.ini update /root/git/jenkins-job-builder/jenkins-job-builder/jobs/jobs.yaml"
+delete="jenkins-jobs --conf $HOME/git/jenkins-job-builder/jenkins_jobs.ini delete $HOME/git/jenkins-job-builder/jobs/jobs.yaml"
+jjb="jenkins-jobs --conf $HOME/git/jenkins-job-builder/jenkins_jobs.ini update $HOME/git/jenkins-job-builder/jobs/jobs.yaml"
 
-cd /root/git/jenkins-job-builder/jobs/
+cd $HOME/git/jenkins-job-builder/jobs/
 
 $delete
+sudo git pull
 $jjb
 
 echo $jjb
