fa8212d4837f2dcd474478dc3a3b31c2f65a0ca8
Divakar <dvkr22@gmail.com>
2023-06-25 22:38:26 +0530

    added

--- jobupdate.sh
+++ jobupdate.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-delete="jenkins-jobs --conf $HOME/git/jenkins-job-builder/jenkins_jobs.ini delete $HOME/git/jenkins-job-builder/jobs/jobs.yaml"
-jjb="jenkins-jobs --conf $HOME/git/jenkins-job-builder/jenkins_jobs.ini update $HOME/git/jenkins-job-builder/jobs/jobs.yaml"
+jjb="jenkins-jobs --flush-cache --ignore-cache --conf $HOME/git/jenkins-job-builder/jenkins_jobs.ini update --delete-old $HOME/git/jenkins-job-builder/jobs/jobs.yaml"
 
 cd $HOME/git/jenkins-job-builder/jobs/
 
-$delete
 sudo git pull
 $jjb
 
